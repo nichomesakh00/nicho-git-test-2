38d9a44173ee6b4c26fa57ae4767853935ff7c6b
Nicholas Mesakhasianki <nichomesakh00@gmail.com>
2024-06-30 03:04:16 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 </head>
 <body>
     <h1>Hello World!</h1>
-    <p>lorem</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, beatae, omnis modi voluptatem reiciendis velit explicabo sit, blanditiis consequatur soluta officia quo quaerat illum tempore quasi possimus placeat molestiae iure?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
